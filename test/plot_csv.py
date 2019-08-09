2f0d00e8e1e5c79225a44ea6459abec5d7ee86f5
FragrantDark <qi.5f20@outlook.com>
2019-08-09 16:16:02 +0800

    test/plot_csv

--- test/plot_csv.py
+++ test/plot_csv.py
@@ -3,7 +3,6 @@ import tushare as ts
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
-import os
 
 
 def get_daily(bdt, edt):
@@ -35,22 +34,58 @@ def file2df(filename, cols):
     return pd.read_csv(filename, usecols=cols)
 
 
-def main_market(df, code):
+def tsc_market(df, code):
     """
-    品种主力合约行情
+    1. 品种主力合约行情
+    2. 品种持仓量加权行情
     :param df:
     :param code:
     :return:
     """
+    code = code.upper()
+    ddic = {}   # ddic[date] = [(code, price, oi)]
+    for i in range(len(df)):
+        v = df.loc[i]
+        tsc = v['ts_code']
+        if v['vol'] == 0 or tsc.find(code) != 0 or len(tsc)-len(code) < 8:
+            continue
+        dt = v['trade_date']
+        print('%s\t%s' % (dt, tsc))
+        if dt not in ddic:
+            ddic[dt] = []
+        ddic[dt].append((tsc, float(v['settle']), float(v['oi'])))
 
+    rr = []     # (date, main_prc, weight_prc, main_code)
+    for dt in ddic:
+        mx_oi, mx_prc, mx_code = 0, 0.0, ''
+        sm_oi, sm_prc = 0, 0.0
+        for t in ddic[dt]:
+            sm_oi += t[2]
+            sm_prc += t[2] * t[1]
+            if t[2] > mx_oi:
+                mx_oi = t[2]
+                mx_prc = t[1]
+                mx_code = t[0]
+        rr.append((dt, mx_prc, sm_prc/sm_oi, mx_code))
 
-def weight_market(df, code):
-    """
-    品种持仓量加权行情
-    :param df:
-    :param code:
-    :return:
-    """
+    rr = sorted(rr, key=lambda x: x[0])
+    print(rr)
+
+    rng = []    # (b, e, main_code)
+    bpos = 0
+    for i in range(1, len(rr)):
+        if rr[i][-1] != rr[i-1][-1]:
+            rng.append((bpos, i, rr[i-1][-1]))
+            bpos = i
+    rng.append((bpos, len(rr), rr[-1][-1]))
+    print(rng)
+
+    main_mkt = [x[1] for x in rr]
+    mres = []   # (x, y, code)
+    for t in rng:
+        mres.append((list(range(t[0], t[1])), main_mkt[t[0]:t[1]], t[2]))
+
+    return mres, [x[2] for x in rr]
 
 
 if __name__ == '__main__':
@@ -59,10 +94,11 @@ if __name__ == '__main__':
 
     df = file2df(fn, ['ts_code', 'trade_date', 'settle', 'vol', 'oi'])
 
-    mmkt = main_market(df, 'a')
-    wmkt = weight_market(df, 'a')
+    mmkt, wmkt = tsc_market(df, 'a')
+
     fig = plt.figure(figsize=(10, 6))
-    plt.plot(mmkt, label='a main', color='blue')
     plt.plot(wmkt, label='a wght', color='red')
+    for tpl in mmkt:
+        plt.plot(tpl[0], tpl[1], label=tpl[2])
     plt.legend()
     plt.show()
